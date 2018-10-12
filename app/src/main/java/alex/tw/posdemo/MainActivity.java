294f96722797324af939f40e7e65cbfb26c27801
Yun8207 <alexs880121@gmail.com>
2018-10-12 22:37:55 +0800

    v1

---
+++ app/src/main/java/alex/tw/posdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package alex.tw.posdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoSelect(View view){
+        Intent intent = new Intent(this, SelectionActivity.class);
+        startActivity(intent);
+
+    }
+}
